d7bde62a21265c07d8705d3ee504647efb74f13d
Mohit Gupta <mohitgupta0000000003@gmail.com>
2026-02-01 19:11:23 +0530

    Describe your change

--- portfolio-engine.js
+++ portfolio-engine.js
@@ -118,7 +118,7 @@ function createStrategyCard(strategy) {
                         <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/>
                         </svg>
-                        Holdings <br> (Jan-26)
+                        Holdings <br> (Feb-26)
                     </a>
                 </div>
             </div>
